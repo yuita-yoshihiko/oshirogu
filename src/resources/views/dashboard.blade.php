846b909d50b8e69d21a621f46879dd0e397899ac
yuita-yoshihiko <soup998211@gmail.com>
2023-06-01 19:48:31 +0900

    ダッシュボードに投稿一覧が表示されるように実装

--- src/resources/views/dashboard.blade.php
+++ src/resources/views/dashboard.blade.php
@@ -11,14 +11,14 @@
     <section class="text-gray-600 body-font">
       <div class="container px-5 py-24 mx-auto">
         <div class="flex flex-wrap -m-4">
-          {{-- @foreach($posts as $post) --}}
+          @foreach($posts as $post)
             <div class="p-4 md:w-1/3">
               <div class="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
-                <img class="lg:h-48 md:h-36 w-full object-cover object-center" src="https://dummyimage.com/720x400" alt="blog">
+                <img class="lg:h-48 md:h-36 w-full object-cover object-center" src="/storage/images/{{ $post->image_path }}" alt="blog">
                 <div class="p-6">
                   <h2 class="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">CATEGORY</h2>
-                  <h1 class="title-font text-lg font-medium text-gray-900 mb-3">The Catalyzer</h1>
-                  <p class="leading-relaxed mb-3">Photo booth fam kinfolk cold-pressed sriracha leggings jianbing microdosing tousled waistcoat.</p>
+                  <h1 class="title-font text-lg font-medium text-gray-900 mb-3"> {{ $post->title }} </h1>
+                  <p class="leading-relaxed mb-3"> {{ $post->comment }} </p>
                   <div class="flex items-center flex-wrap ">
                     <a class="text-indigo-500 inline-flex items-center md:mb-2 lg:mb-0">Learn More
                       <svg class="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round">
@@ -41,7 +41,7 @@
                 </div>
               </div>
             </div>
-          {{-- @endforeach --}}
+          @endforeach
         </div>
       </div>
     </section>
--- src/routes/web.php
+++ src/routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
+use App\Http\Controllers\DashboardController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,9 +20,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
