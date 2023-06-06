1b9b6d45ec1b2d82894b5399c5e7431e07d705cd
yuita-yoshihiko <soup998211@gmail.com>
2023-06-06 13:52:34 +0900

    デザインの整理

--- src/resources/views/dashboard.blade.php
+++ src/resources/views/dashboard.blade.php
@@ -20,7 +20,7 @@
                   <h1 class="title-font text-lg font-medium text-gray-900 mb-3"> {{ $post->title }} </h1>
                   <p class="leading-relaxed mb-3"> {{ $post->comment }} </p>
                   <div class="flex items-center flex-wrap ">
-                    <span class="text-gray-400 mr-3 inline-flex items-center lg:ml-auto md:ml-0 ml-auto leading-none text-sm pr-3 py-1 border-r-2 border-gray-200">
+                    <span class="text-gray-400 mr-3 inline-flex items-center lg:ml-auto md:ml-0 ml-auto leading-none text-sm pr-3 py-1">
                       <div class='btn-group'>
                         @if ($isAuthenticated && $post->isNotPostOwner)
                             @if ($post->isLiked)
@@ -34,11 +34,6 @@
                             @endif
                         @endif
                       </div>
-                    </span>
-                    <span class="text-gray-400 inline-flex items-center leading-none text-sm">
-                      <svg class="w-4 h-4 mr-1" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round" viewBox="0 0 24 24">
-                        <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"></path>
-                      </svg>6
                     </span>
                   </div>
                 </div>
